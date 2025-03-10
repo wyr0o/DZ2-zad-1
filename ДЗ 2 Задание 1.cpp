efa433000fb784c92cf8f13f35ac28054dc6a290
wyr0o <brykin-2006@mail.ru>
2025-03-10 19:47:24 +0300

    Add files via upload

---
+++ ДЗ 2 Задание 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    int num ;
+    cout<< "введите число: " <<endl;
+    cin >> num;
+    cout<< "вы ввели: "<< endl ;
+    cout<< num;
+    
+    
+}
