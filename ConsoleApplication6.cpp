cee2a516b7f44ccc8592c3c07f452779df71bdbf
wyr0o <brykin-2006@mail.ru>
2025-03-11 08:24:21 +0300

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string text;
+	cout << "Введите текст: "<< endl;
+	getline(cin, text);
+	cout << "Вы ввели: " << endl;
+	cout << text;
+}
